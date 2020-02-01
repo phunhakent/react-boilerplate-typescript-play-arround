e66bf25f97679813e83ab9953cd5c81a7cff047c
Phu Nguyen <phu.nguyen@murcul.tech>
2020-02-01 10:31:31 +0700

    Initial Commit

---
+++ app/configureStore.ts
@@ -0,0 +1,79 @@
+/**
+ * Create the store with dynamic reducers
+ */
+
+import { applyMiddleware, createStore } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
+import createSagaMiddleware from 'redux-saga';
+import createReducer from './reducers';
+import { InjectedStore, ApplicationRootState } from 'types';
+import { History } from 'history';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+export default function configureStore(
+  initialState: ApplicationRootState | {} = {},
+  history: History,
+) {
+  const reduxSagaMonitorOptions = {};
+  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+
+  const middlewares = [sagaMiddleware, routerMiddleware(history)];
+
+  let enhancer = applyMiddleware(...middlewares);
+
+  // If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
+  /* istanbul ignore next */
+  if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
+    enhancer = composeWithDevTools(enhancer);
+  }
+
+  // NOTE: Uncomment the code below to restore support for Redux Saga
+  // Dev Tools once it supports redux-saga version 1.x.x
+  // if (window.__SAGA_MONITOR_EXTENSION__)
+  //   reduxSagaMonitorOptions = {
+  //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
+  //   };
+
+  // Create the store with two middlewares
+  // 1. sagaMiddleware: Makes redux-sagas work
+  // 2. routerMiddleware: Syncs the location/URL path to the state
+
+  const rootReducer = createReducer();
+
+  const persistConfig: PersistConfig<ApplicationRootState> = {
+    key: 'root',
+    storage,
+  };
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+  const store = createStore(
+    persistedReducer,
+    initialState,
+    enhancer,
+  ) as InjectedStore;
+
+  const persistor = persistStore(store);
+
+  // Extensions
+  store.runSaga = sagaMiddleware.run;
+  store.injectedReducers = {}; // Reducer registry
+  store.injectedSagas = {}; // Saga registry
+  store.persistor = persistor;
+  store.persistConfig = persistConfig;
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = createReducer(store.injectedReducers);
+      store.replaceReducer(
+        persistReducer(store.persistConfig, nextRootReducer),
+      );
+    });
+  }
+
+  return store;
+}
